8d574dd1fcf7dcb2d68aa189d99412b5cc11c9bb
Richard Kwok <richardjkwok@gmail.com>
2017-11-05 23:05:56 -0800

    Add spin about earth normal for extra degree of freedom

--- cubesat.m
+++ cubesat.m
@@ -6,6 +6,7 @@ classdef cubesat < satellite
         x % the other side length a
         y % the other side length b (a and b basically interchangeable)
         
+        spin % rads/s, because the cubesat can spin about the z axis freely
     end
     
     methods
@@ -16,10 +17,11 @@ classdef cubesat < satellite
             self.x = x;
             self.y = y;
             self.z = z;
+            self.spin = 0;
             
         end
         
-        function y = illuminated_area(self, angle)
+        function y = illuminated_area(self, angle, spin_angle)
             
             eclipse_max = self.eclipse_angle;
             eclipse_min = 2*pi - self.eclipse_angle;
@@ -32,24 +34,36 @@ classdef cubesat < satellite
             
             % a1 is the top surface facing away from earth
             a1 = self.x*self.y; 
-            na = [-1; 0];
+            na = [-1; 0; 0];
             
             % a2 is one of the side surfaces
             a2 = self.x*self.z;
-            nb = [0; 1];
-            nc = [0; -1];
+            nb = [0; 1; 0];
+            nc = [0; -1; 0];
             
             % a3 is the other side surface, important if x!=y
             a3 = self.y*self.z;
+            nd = [0; 0; 1];
+            ne = [0; 0; -1];
             
-            % rotation matrix
-            A = [cos(angle) sin(angle); -sin(angle) cos(angle)];
+            % spin rotation about axis normal to earth
+            spin_matrix = rotation(spin_angle, 1, 0, 0);
+            
+            nb = spin_matrix*nb;
+            nc = spin_matrix*nc;
+            nd = spin_matrix*nd;
+            ne = spin_matrix*ne;
+            
+            % major rotation about earth
+            orbit_matrix = rotation(angle, 0, 0, 1);
             
             % weighted sum of surface areas based on what proportion of
-            % each surface normal is antiparallel with the sunbeam [1; 0]
-            y = max([1 0]*A*na,0)*a1 ...
-              + max([1 0]*A*nb,0)*a2 ...
-              + max([1 0]*A*nc,0)*a2;
+            % each normal is antiparallel with the sunbeam [1; 0, 0]
+            y = max([1 0 0]*orbit_matrix*na,0)*a1 ...
+              + max([1 0 0]*orbit_matrix*nb,0)*a2 ...
+              + max([1 0 0]*orbit_matrix*nc,0)*a2 ...
+              + max([1 0 0]*orbit_matrix*nd,0)*a3 ...
+              + max([1 0 0]*orbit_matrix*ne,0)*a3;
 
         end
         
--- main.m
+++ main.m
@@ -2,8 +2,12 @@
 % define our satellite as 1 kg and 0.1m*0.1m*0.2m at a 2000km height
 sfusat = cubesat(1, 0.1, 0.1, 0.2, 2000);
 
+% set the spin rate of the satellite in rad/s
+sfusat.spin = 0;
+
 % only one state variable, initially 0 halfway through eclipse region
 angle = 0;
+spin_angle = 0;
 
 % dt and max_time in seconds
 dt = 60;
@@ -15,11 +19,14 @@ y = [];
 
 for t = 0:dt:max_time
     
-    x = [x; t];
-    y = [y; sfusat.illuminated_area(angle)];
+    x = [x; (t/60)/60];
+    y = [y; sfusat.illuminated_area(angle, spin_angle)*100*100];
     
     angle = mod(angle + sfusat.angular_v*dt, 2*pi);
+    spin_angle = mod(spin_angle + sfusat.spin*dt, 2*pi);
     
 end
 
 plot(x, y);
+xlabel('time (hours)')
+ylabel('effective area (cm^2)')
