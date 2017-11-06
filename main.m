64450bdef938cddd8f821a1721cf929fe5f7f8b6
Richard Kwok <richardjkwok@gmail.com>
2017-11-05 21:40:05 -0800

    Implement basic simulation of illuminated area over time using cubesat class

---
+++ main.m
@@ -0,0 +1,25 @@
+
+% define our satellite as 1 kg and 0.1m*0.1m*0.2m at a 2000km height
+sfusat = cubesat(1, 0.1, 0.1, 0.2, 2000);
+
+% only one state variable, initially 0 halfway through eclipse region
+angle = 0;
+
+% dt and max_time in seconds
+dt = 60;
+max_time = 24*60*60;
+
+% arrays to save data for plotting
+x = [];
+y = [];
+
+for t = 0:dt:max_time
+    
+    x = [x; t];
+    y = [y; sfusat.illuminated_area(angle)];
+    
+    angle = angle + sfusat.angular_v*dt;
+    
+end
+
+plot(x, y);
